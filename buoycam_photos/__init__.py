439da1a5a0a7376a3d5930db2c7c5cd3f1476f60
Gregor Weichbrodt <gregorweichbrodt@gmail.com>
2021-11-09 18:04:22 +0100

    update module exports

--- buoycam_photos/__init__.py
+++ buoycam_photos/__init__.py
@@ -1,7 +1,2 @@
 from buoycam_photos.get_station_names import get_station_names
 from buoycam_photos.get_station_images import get_station_images
-
-__all__ = [
-    'get_station_names',
-    'get_station_images'
-]
